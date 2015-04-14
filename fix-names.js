533373c189e04298c1e6ab2a707fe4b253c698be
Alex Olshansky <i@creagenics.com>
2015-04-14 16:26:11 +0200

    Initial commit

---
+++ fix-names.js
@@ -0,0 +1,13 @@
+var users = {
+  'alex.olsh': 'clbn'
+};
+
+var userLinks = $('h5, h6, .UFICommentContent').find('a[href^="https://www.facebook.com/"]');
+
+userLinks.map(function() {
+  var href = $(this).attr('href');
+  var username = href.match(/https:\/\/www\.facebook\.com\/([\w\.]+)(\/|\?|$)/)[1];
+  if (users[username]) {
+    $(this).append(' <span class="facefeedik-name">(' + users[username] + ')</span>');
+  }
+});
