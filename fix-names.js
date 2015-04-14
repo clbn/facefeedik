8a3974b448f260025e6d8de925c903a0272c8b06
Alex Olshansky <i@creagenics.com>
2015-04-14 17:27:13 +0200

    Add documentLoaded and MutationObserver

--- fix-names.js
+++ fix-names.js
@@ -1,13 +1,17 @@
-var users = {
-  'alex.olsh': 'clbn'
-};
+registerAction(function (node) {
+  node = node || document.body;
 
-var userLinks = $('h5, h6, .UFICommentContent').find('a[href^="https://www.facebook.com/"]');
+  var users = {
+    'alex.olsh': 'clbn'
+  };
 
-userLinks.map(function() {
-  var href = $(this).attr('href');
-  var username = href.match(/https:\/\/www\.facebook\.com\/([\w\.]+)(\/|\?|$)/)[1];
-  if (users[username]) {
-    $(this).append(' <span class="facefeedik-name">(' + users[username] + ')</span>');
-  }
+  var userLinks = $(node).find('h5, h6, .UFICommentContent').find('a[href^="https://www.facebook.com/"]');
+
+  userLinks.map(function () {
+    var href = $(this).attr('href');
+    var username = href.match(/https:\/\/www\.facebook\.com\/([\w\.]+)(\/|\?|$)/)[1];
+    if (users[username]) {
+      $(this).append(' <span class="facefeedik-name">(' + users[username] + ')</span>');
+    }
+  });
 });
---
+++ init.js
@@ -0,0 +1,37 @@
+var registerAction = function(action) {
+  actions.push(action);
+};
+
+var toArray = function(nl) {
+  return Array.prototype.slice.call(nl);
+};
+
+var documentLoaded = new Promise(function (resolve) {
+  if (document.readyState === 'complete' || document.readyState === 'interactive') {
+    setTimeout(resolve, 0);
+  } else {
+    document.addEventListener("DOMContentLoaded", resolve);
+  }
+});
+
+var observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.addedNodes.length == 0) {
+      return;
+    }
+    toArray(mutation.addedNodes).forEach(function(node) {
+      if (node.nodeType == Node.ELEMENT_NODE) {
+        actions.forEach(function(action) {
+          action(node);
+        });
+      }
+    });
+  });
+});
+
+var actions = [];
+
+documentLoaded.then(function() {
+  actions.forEach(function(action) { action(); });
+  observer.observe(document.body, {childList: true, subtree: true});
+});
